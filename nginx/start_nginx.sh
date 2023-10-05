cd7721fdbcaa163e720f69a166b9b141cc7f8561
Vuyisile Ndlovu <terrameijar@gmail.com>
2023-10-05 14:08:50 +0200

    ci: Choose TLS cert based on environment
    Remove self signed certificates from repo

--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ENVIRONMENT="${ENVIRONMENT:-production}"
+ENVIRONMENT="${ENVIRONMENT:-development}"
 
 PRODUCTION_CONFIG="nginx.conf"
 DEVELOPMENT_CONFIG="nginx-dev.conf"
@@ -9,6 +9,17 @@ if [ "$ENVIRONMENT" = "production" ]; then
     echo "Starting nginx using production config"
     nginx -c /etc/nginx/$PRODUCTION_CONFIG -g "daemon off;"
 else
+
+    # Create a self-signed certificate if one doesn't exist
+    if [ ! -f /etc/nginx/ssl/invoices_fullchain_dev.pem ]; then
+        echo "Creating self-signed certificate"
+        mkdir -p /etc/nginx/ssl
+        openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+            -keyout /etc/nginx/ssl/invoices_privkey.pem \
+            -out /etc/nginx/ssl/invoices_fullchain_dev.pem \
+            -subj "/CN=localhost"
+    fi
+
     echo "Starting nginx using development config"
     nginx -c /etc/nginx/$DEVELOPMENT_CONFIG -g "daemon off;"
 fi
