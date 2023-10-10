8d5147cee228264d5829e1ae4a9746d4291e60d3
Vuyisile Ndlovu <terrameijar@gmail.com>
2023-10-10 04:46:42 +0200

    Add initial form messages and styles

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib import messages
 
 from .forms import CustomUserCreationForm
 
@@ -8,3 +9,7 @@ class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
+
+    def form_valid(self, form):
+        messages.success(self.request, "Account created successfully")
+        return super().form_valid(form)
